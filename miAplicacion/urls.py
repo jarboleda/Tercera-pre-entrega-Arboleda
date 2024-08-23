48e7a12235a10aab82b1a6ed60dae23c8e0b6973
Javier Arboleda Parra <javier.arboleda@gmail.com>
2024-08-22 22:44:07 -0500

    Cambios en los formularios y vistas

--- miAplicacion/urls.py
+++ miAplicacion/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.Inicio),
     path('inicio/', views.Inicio, name='inicio'),
-    path('grupos/', views.Grupos, name='grupos'),
-    path('supervisores/', views.Supervisores, name='supervisores'),
-    path('usuarios/', views.Usuarios, name='usuarios'),
+    path('gruposList/', views.gruposList, name='gruposList'),
+    path('supervisoresList/', views.supervisoresList, name='supervisoresList'),
+    path('usuariosList/', views.usuariosList, name='usuariosList'),
     path('gruposForm/', views.gruposForm, name='gruposForm'),
     path('supervisoresForm/', views.supervisoresForm, name='supervisoresForm'),
     path('usuariosForm/', views.usuariosForm, name='usuariosForm'),
--- miAplicacion/views.py
+++ miAplicacion/views.py
@@ -1,23 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import random
 from datetime import date
+from miAplicacion.models import Grupos, Supervisores, Usuarios
 
 # Create your views here.
 
 def Inicio(req):
     return render(req, "miaplicacion/index.html")
 
-def Grupos(req):
-    return render(req, "miaplicacion/grupos.html")
+def gruposList(req):
+    gruposTodos = Grupos.objects.all()
+    return render(req, "miaplicacion/grupos.html",{'gruposTodos': gruposTodos})
 
-def Supervisores(req):
+def supervisoresList(req):
     return render(req, "miaplicacion/supervisores.html")
 
-def Usuarios(req):
+def usuariosList(req):
     return render(req, "miaplicacion/usuarios.html")
 
 def gruposForm(req):
+
+    if req.method == 'POST':
+        cod = str(random.randint(10000,99999))
+        sta = "A"
+        grupo = Grupos(codigo=cod, nombre=req.POST['nombre'], idioma=req.POST['idioma'], estado=sta, creacion=date.today())
+        grupo.save()
+
+        return redirect('gruposList')
+
     return render(req, 'miaplicacion/gruposForm.html')
 
 def supervisoresForm(req):
@@ -25,13 +36,3 @@ def supervisoresForm(req):
 
 def usuariosForm(req):
     return render(req, 'miaplicacion/usuariosForm.html')
-
-
-def agregaGrupo(req, nom, idi):
-
-    cod = str(random.randint(10000,99999))
-    sta = "A"
-    grupo = Grupos(codigo=cod, nombre=nom, idioma=idi, estado=sta, creacion=date.today())
-    grupo.save()
-
-    return HttpResponse("Se agregó el grupo")
