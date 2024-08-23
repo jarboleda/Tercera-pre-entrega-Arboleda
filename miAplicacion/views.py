7d5a56698634e3bc820251a723cd564acdd9b7b3
Javier Arboleda Parra <javier.arboleda@gmail.com>
2024-08-22 23:01:57 -0500

    Modificaciones a los formularios y vistas

--- miAplicacion/views.py
+++ miAplicacion/views.py
@@ -11,20 +11,23 @@ def Inicio(req):
 
 def gruposList(req):
     gruposTodos = Grupos.objects.all()
-    return render(req, "miaplicacion/grupos.html",{'gruposTodos': gruposTodos})
+    return render(req, "miaplicacion/grupos.html", {'gruposTodos': gruposTodos})
 
 def supervisoresList(req):
-    return render(req, "miaplicacion/supervisores.html")
+    supervisoresTodos = Supervisores.objects.all()
+    return render(req, "miaplicacion/supervisores.html", {'supervisoresTodos': supervisoresTodos})
 
 def usuariosList(req):
-    return render(req, "miaplicacion/usuarios.html")
+    usuariosTodos = Usuarios.objects.all()
+    return render(req, "miaplicacion/usuarios.html", {'usuariosTodos': usuariosTodos})
 
 def gruposForm(req):
 
     if req.method == 'POST':
         cod = str(random.randint(10000,99999))
         sta = "A"
-        grupo = Grupos(codigo=cod, nombre=req.POST['nombre'], idioma=req.POST['idioma'], estado=sta, creacion=date.today())
+        cre = date.today()
+        grupo = Grupos(codigo=cod, nombre=req.POST['nombre'], idioma=req.POST['idioma'], estado=sta, creacion=cre)
         grupo.save()
 
         return redirect('gruposList')
@@ -32,7 +35,27 @@ def gruposForm(req):
     return render(req, 'miaplicacion/gruposForm.html')
 
 def supervisoresForm(req):
+
+    if req.method == 'POST':
+        cod = str(random.randint(10000,99999))
+        sta = "A"
+        cre = date.today()
+        grupo = Supervisores(codigo=cod, nombres=req.POST['nombre'], idioma=req.POST['idioma'], email=req.POST['email'], estado=sta, creacion=cre)
+        grupo.save()
+
+        return redirect('supervisoresList')
+    
     return render(req, 'miaplicacion/supervisoresForm.html')
 
 def usuariosForm(req):
+
+    if req.method == 'POST':
+        cod = str(random.randint(10000,99999))
+        sta = "A"
+        cre = date.today()
+        grupo = Usuarios(codigo=cod, nombres=req.POST['nombre'], idioma=req.POST['idioma'], estado=sta, creacion=cre)
+        grupo.save()
+
+        return redirect('usuariosList')
+    
     return render(req, 'miaplicacion/usuariosForm.html')
