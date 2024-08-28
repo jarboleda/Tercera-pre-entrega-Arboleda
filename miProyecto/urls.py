382d68a67e39bf3018169bda644a2314afd1536a
Javier Arboleda Parra <javier.arboleda@gmail.com>
2024-08-28 17:11:36 -0500

    Cambios en las vistas html

--- miProyecto/urls.py
+++ miProyecto/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from miProyecto import views 
-from miAplicacion import views
+from miAplicacion.views import Inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
